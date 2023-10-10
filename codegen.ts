d8516df02dad26e8a2d78378c9bf1d7ceb295a5d
farhan rafid syauqi <farhanrafid97@gmail.com>
2023-10-10 15:14:17 +0700

    feat: setup apollo and codegen

---
+++ codegen.ts
@@ -0,0 +1,14 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'https://wpe-hiring.tokopedia.net/graphql',
+  documents: ['./graphql/**/*.graphql'],
+  generates: {
+    'src/gql/file.ts': {
+      plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
+    },
+  },
+};
+
+export default config;
